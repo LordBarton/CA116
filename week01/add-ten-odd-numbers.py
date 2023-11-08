cd6e74f117dea7bd461a8228e18c1c06ca83410b
LordBarton <117317794+LordBarton@users.noreply.github.com>
2023-11-08 04:22:17 +0000

    Create add-ten-odd-numbers.py

---
+++ week01/add-ten-odd-numbers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+total = 0
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+a = int(input())
+total += ((a % 2) * a)
+
+print(total)
