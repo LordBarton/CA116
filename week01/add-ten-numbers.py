d9019859ba63de48cfcf97a0dcf9b23599bfdac0
LordBarton <117317794+LordBarton@users.noreply.github.com>
2023-11-08 04:20:57 +0000

    Create add-ten-numbers.py
    labsheet 1b

---
+++ week01/add-ten-numbers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+total = 0
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+a = int(input())
+total += a
+
+print(total)
